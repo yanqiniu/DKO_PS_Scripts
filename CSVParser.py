efff37dad9814ae4c71fea1507cc9adf3ddd0a13
Yanqi Roy Niu (PC) <yanqi.roy.niu@gmail.com>
2017-04-17 14:19:44 -0700

    Cleaning debug message, untabify.

--- CSVParser.py
+++ CSVParser.py
@@ -9,41 +9,41 @@ destFilePath = "Cards.json"
 
 def main():
 
-	destFile = open(destFilePath, "w")
-	destFile.write("")
+    destFile = open(destFilePath, "w")
+    destFile.write("")
 
-	lineIndex = 0
-	headerList = []
+    lineIndex = 0
+    headerList = []
 
-	toWrite = "{ \"cards\":["
+    toWrite = "{ \"cards\":["
 
-	with open(srcFilePath) as f:
-		for line in f:
-			print line
-			if lineIndex == 0:
-				# this is the first line of the file, aka headers
-				headers = line.split(",")
-			else:
-				# this is an actual data line
-				toWrite += processSlgLine(headers, line)
-			lineIndex = lineIndex + 1
-	toWrite = toWrite[:-3]
-	toWrite += "]}"
-	FileHelper.appendLineTo(toWrite, destFilePath, False)
+    with open(srcFilePath) as f:
+        for line in f:
+            if lineIndex == 0:
+                # this is the first line of the file, aka headers
+                headers = line.split(",")
+            else:
+                # this is an actual data line
+                print(" - Processing card %s\r" % lineIndex )
+                toWrite += processSlgLine(headers, line)
+            lineIndex = lineIndex + 1
+    toWrite = toWrite[:-3]
+    toWrite += "]}"
+    FileHelper.appendLineTo(toWrite, destFilePath, False)
+    print("Finished parsing csv file." )
 
 def processSlgLine(headers, line):
-	dataList = line.split(",")
-	toWrite = "\r\n{\r\n"
-	index = 0
-	for name in headers:
-		name = FileHelper.stripnl(name)
-		dataContent = FileHelper.stripnl(dataList[index])
-		print dataContent
-		toWrite += "\"{0}\" : \"{1}\",\r\n".format(name, dataContent)
-		index += 1
-	toWrite = toWrite[:-3]
-	toWrite += "\r\n},\r\n"
+    dataList = line.split(",")
+    toWrite = "\r\n{\r\n"
+    index = 0
+    for name in headers:
+        name = FileHelper.stripnl(name)
+        dataContent = FileHelper.stripnl(dataList[index])
+        toWrite += "\"{0}\" : \"{1}\",\r\n".format(name, dataContent)
+        index += 1
+    toWrite = toWrite[:-3]
+    toWrite += "\r\n},\r\n"
 
-	return toWrite
+    return toWrite
 
 main()
